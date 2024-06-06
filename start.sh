6a240d1206514413cc610b9ada5c97d3fa41f9cf
Verma7890 <137143631+Verma7890@users.noreply.github.com>
2024-06-06 16:44:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/Verma7890/Spider-Auto-Filter-Bot /Spider-Auto-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Spider-Auto-Filter-Bot
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
